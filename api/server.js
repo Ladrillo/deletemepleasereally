7476abcdc53a6c03dfb5fae125ae4ec444ea2af6
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-11 17:36:07 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -1,16 +1,22 @@
 const express = require('express')
-
+const path = require('path')
 const server = express()
 
 server.use(express.json())
+
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 if (!process.env.NODE_ENV === 'production') {
   const cors = require('cors')
   server.use(cors())
 }
 
+server.get('/api', (req, res) => {
+  res.json({ message: 'api up' })
+})
+
 server.use('*', (req, res) => {
-  res.send('<h1>SUCCESS</h1>')
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 module.exports = server
