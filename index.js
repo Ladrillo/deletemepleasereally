75ec4a4e3754c6e8ab2a89f16b30e37a0b87c11d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-11 17:44:05 +0100

    wip

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-require('dotenv').config()
+// require('dotenv').config()
+// at the top of index.js
+const dotenv = require('dotenv')
+dotenv.config()
+
 const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 4000
 const path = require('path')
-const server = require('./api/server')
 
-const PORT = process.env.PORT || 4000
+console.log(__dirname)
 
-server.use(express.static(path.join(__dirname, 'client/build')))
-server.use('*', (req, res) => {
+console.log(process.env.LADY)
+console.log(process.env.FOO)
+console.log(process.env.ALWAYS)
+console.log(process.env.PORT)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+// api, routers
+app.use('/api/*', (_, res) => {
+  res.json({ data: 'web 36 rocks'})
+})
+
+app.use('*', (_, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
-server.listen(PORT, () => {
-  console.log('success listening on', PORT)
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
 })
